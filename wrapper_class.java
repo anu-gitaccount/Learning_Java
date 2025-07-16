9bb5c12afbe1890f53d972e34bcec835c86981f5
anu-gitaccount <anuragsinghsep26@gmail.com>
2025-07-16 23:39:35 +0530

    wrapper class

---
+++ wrapper_class.java
@@ -0,0 +1,25 @@
+public class wrapper_class {
+    public static  void main(String[] args) {
+
+        // Wrapper class = alows a way to use primitive data type as refrence datetypes
+        //                 reference datatypes has useful methods
+
+        // Primitive                // Wrapper
+        // int                      // Integer
+        // boolean                  // Boolean
+        // double                   // Double
+
+
+        // autoboxing = automatic conversion that javac makes primitive to refrence
+        // unboxing = reverse automatic conversion
+
+        // Refrence data type var
+        Integer num = 10;
+        Character ch = 'a';
+        Boolean valid = false;
+        Double dec = 10.000;
+
+
+
+    }
+}
