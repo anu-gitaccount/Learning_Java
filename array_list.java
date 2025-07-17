f741fe8a49b0f20f007b8e3d398387f63052f6b1
anu-gitaccount <anuragsinghsep26@gmail.com>
2025-07-17 21:37:42 +0530

    Container

---
+++ array_list.java
@@ -0,0 +1,32 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class array_list {
+    public static void main(String[] args) {
+
+        // ArrayList = like a Vector
+        //              resizable array - stores refrence data type
+
+
+        //ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        ArrayList<String> food = new ArrayList<String>();
+
+        food.add("burger");
+        food.add("pizza");
+        food.add("bread");
+
+        // replace a element using set(index, new_element);
+        food.set(0, "noodles");
+
+        // .remove(index)  removes the element from the index
+        food.remove(1);
+
+        // .clear()  removes all the element
+            food.clear();
+
+        for(int i = 0; i < food.size(); i++) {
+            System.out.println(food.get(i));
+        }
+    }
+}
